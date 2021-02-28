bd313209012a427ff17467501ecae31da6f75feb
Andre Saimon <andresousa.vr@gmail.com>
2021-02-28 17:50:45 -0300

    first commit

---
+++ src/controllers/AnswerController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { AppError } from "../errors/AppError";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+
+
+
+class AnswerController {
+
+    // http://localhost:3333/answers/1?u=488378fb-7ae0-4c96-8989-2adf80cdccf6
+    /**
+     * 
+        Route Params => Parâmetros que compõe a rota
+        routes.get("/answers/:value")
+
+        Query Params => Busca, Paginação, não obrigatórios
+        ?
+        chave=valor
+     */
+    async execute(request: Request, response: Response) {
+        const { value } = request.params;
+        const { u } = request.query;
+
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+        const surveyUser = await surveysUsersRepository.findOne({
+            id: String(u)
+        });
+
+        if(!surveyUser) {
+            throw new AppError("Survey User does not exists!");
+        }
+
+        surveyUser.value = Number(value);
+
+        await surveysUsersRepository.save(surveyUser);
+
+        return response.json(surveyUser);
+    }
+}
+
+export { AnswerController }
